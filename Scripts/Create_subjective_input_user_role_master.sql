46726f9a78345435aaa599cbc77ccc598c18b2e0
sgarikapati <sgarikapati@kingsettcapital.com>
2026-08-10 11:59:43 -0400

    User management changes

--- Scripts/Create_subjective_input_user_role_master.sql
+++ Scripts/Create_subjective_input_user_role_master.sql
@@ -1,15 +1,10 @@
--- User Management tables in wh_gold1.subjective_input (snake_case columns).
-
+-- Live warehouse schema uses [status] (A/I). Older drafts used [is_active] (Y/N).
 IF OBJECT_ID(N'[subjective_input].[role_master]', N'U') IS NULL
 BEGIN
     CREATE TABLE [subjective_input].[role_master](
         [role_id] [int] NULL,
         [role_name] [varchar](255) NULL,
-        [is_active] [char](1) NULL,
-        [created_datetime] [datetime2](6) NULL,
-        [created_by] [varchar](100) NULL,
-        [updated_datetime] [datetime2](6) NULL,
-        [updated_by] [varchar](100) NULL
+        [status] [char](1) NULL
     ) ON [PRIMARY];
 END
 GO
